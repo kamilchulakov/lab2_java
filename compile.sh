79bf70b40542c1cf022142044c11fa603e3dd161
ulyanovskk <mr1english4@gmail.com>
2020-11-21 04:02:46 +0300

    I DELETED CLASSES FROM SOURCE

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
-rm -rf classes/* releases/*
+rm -rf classes releases
+mkdir classes
+mkdir releases
 cd source
 javac -cp ".:../lib/Pokemon.jar" -d ../classes lab/attacks/*.java
 javac -cp ".:../lib/Pokemon.jar:lab/attacks" -d ../classes lab/pokemons/*.java
